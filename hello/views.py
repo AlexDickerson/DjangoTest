92ea13389ad02618c203fd32e77583d9a97db4fd
Alex <alex@alexdickerson.com>
2020-04-04 19:41:47 +0000

    Initial Commit

---
+++ hello/views.py
@@ -0,0 +1,23 @@
+import re
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
